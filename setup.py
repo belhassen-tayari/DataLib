c5882648f8e3c2c4f779b7498b75fb626da461fd
unknown <belhassentayari09@gmail.com>
2024-12-06 00:00:15 +0100

    ready to depoly

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="datalib",
-    version="0.1.0",
+    name="datalib_btayari",  # Nom de votre package
+    version="0.1.0",  # Version initiale
+    author="Votre Nom",
+    author_email="votre.email@example.com",
+    description="A Python library for data manipulation and analysis",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/votre_nom_utilisateur/DataLib",  # URL du dépôt GitHub
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        "numpy",
-        "pandas",
-        "matplotlib",
-        "scikit-learn"
+        "numpy>=1.21.0",
+        "pandas>=1.3.0",
+        "matplotlib>=3.4.0",
+        "scikit-learn>=0.24.0"
     ],
-    author="Your Name",
-    description="A Python library for data manipulation and analysis",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires=">=3.7",
 )
